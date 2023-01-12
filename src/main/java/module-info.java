2d390931b6b2de0c2d51d2fe28b5d43c7e23292f
emacri <emacri@educa.madrid.org>
2023-01-12 09:21:05 +0100

    merged game gui with version 0.4

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -12,11 +12,22 @@ module game {
     exports game.demiurge;
     exports game.dungeon;
     exports game.character.exceptions;
+    exports game.conditions;
+    exports game.demiurge.exceptions;
+    exports game.spell;
+    exports loaderManual;
+    exports game.spellContainer;
+    exports game.objectContainer.exceptions;
+    exports game.object;
+    exports wizardNightmare;
 
-//    opens game.gui.main;
-//    opens game.gui.main.common;
-//    opens game.gui.screens.main to javafx.fxml;
-//    opens game.gui.screens.common;
+    exports game.gui.main;
+    exports game.gui.screens.main;
+
+    opens game.gui.main;
+    opens game.gui.main.common;
+    opens game.gui.screens.main to javafx.fxml;
+    opens game.gui.screens.common;
     opens game.dungeon;
     opens game.util;
 
