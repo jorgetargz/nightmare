9502ea353105176260f24475b1bb306fb92d92fa
emacri <emacri@educa.madrid.org>
2023-01-12 08:59:44 +0100

    upgraded to version 0.4

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -11,18 +11,12 @@ module game {
     exports game.util;
     exports game.demiurge;
     exports game.dungeon;
-    exports game.dungeon.spell;
-    exports game.dungeon.structure;
-    exports game.dungeon.structure.exceptions;
-    exports game.dungeon.object.exceptions;
     exports game.character.exceptions;
-    exports game.gui.screens.main;
-    exports wizardNightmare;
 
-    opens game.gui.main;
-    opens game.gui.main.common;
-    opens game.gui.screens.main to javafx.fxml;
-    opens game.gui.screens.common;
+//    opens game.gui.main;
+//    opens game.gui.main.common;
+//    opens game.gui.screens.main to javafx.fxml;
+//    opens game.gui.screens.common;
     opens game.dungeon;
     opens game.util;
 
