0801435c42b840d49445c778d2befdf0a3127f12
Jorge Martín Llorente <jorgemartin99@outlook.com>
2023-01-30 19:06:03 +0100

    lo voy a poner todo en una pantalla haciendo las cosas visibles e invisibles

--- src/main/java/interfaz/screens/common/Screens.java
+++ src/main/java/interfaz/screens/common/Screens.java
@@ -2,13 +2,9 @@ package interfaz.screens.common;
 
 public enum Screens {
 
-    WELCOME(ScreenConstants.FXML_WELCOME_SCREEN_FXML),
-    LOGIN(ScreenConstants.FXML_LOGIN_SCREEN_FXML),
     MAIN("/fxml/mainScreen.fxml"),
     INICIO("/fxml/partida/inicio/pantallaInicio.fxml"),
-    CASAPRINCIPAL("/fxml/casaMago/casaPrincipal.fxml"),
-    ALMACEN("/fxml/casaMago/almacen.fxml"),
-    BIBLIOTECA("/fxml/casaMago/biblioteca.fxml"),
+    CASA("/fxml/casaMago/casaPrincipal.fxml"),
     PANTALLAJUEGO("/fxml/juego/pantallaJuego.fxml"),
     PANTALLAPELEAS("/fxml/juego/pantallaPeleas.fxml"),
     BBDD("/fxml/partida/bbdd/bbdd.fxml");
